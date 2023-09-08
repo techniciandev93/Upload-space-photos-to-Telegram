0ade6a83a1ec9873f178969d419c7c1afdcc858e
anton <anton@adminpc.Dlink>
2023-09-08 18:32:49 +0300

    Изменено наименование переменной images_data на space_images

--- download_epic_images_planet_earth.py
+++ download_epic_images_planet_earth.py
@@ -10,9 +10,9 @@ def download_images_planet_earth(api_key, path):
     nasa_url = f'https://api.nasa.gov/EPIC/api/natural'
     response = requests.get(nasa_url, params=params)
     response.raise_for_status()
-    for images_data in response.json():
-        image = f"{images_data['image']}.png"
-        date = datetime.strptime(images_data['date'], '%Y-%m-%d %H:%M:%S')
+    for space_images in response.json():
+        image = f"{space_images['image']}.png"
+        date = datetime.strptime(space_images['date'], '%Y-%m-%d %H:%M:%S')
         format_date = date.strftime("%Y/%m/%d")
         image_url = f'https://api.nasa.gov/EPIC/archive/natural/{format_date}/png/{image}'
         download_image(image_url, path, image)
